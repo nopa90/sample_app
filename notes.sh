513cc0bae84ca94da261e4d7c0976d3399941ca5
nopa90 <nopa90@gmail.com>
2013-09-01 18:48:45 +0800

    Finish static pages

--- notes.sh
+++ notes.sh
@@ -2,5 +2,11 @@
 #testing framework
 bundle exec rspec spec/requests/static_pages_spec.rb
 
+#commit changes
+git add .
+git commit -m "Finish static pages"
 
+#merge results changes back to main
 
+git checkout master
+git merge static-pages
